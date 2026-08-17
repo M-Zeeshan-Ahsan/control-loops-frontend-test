dc19c6a84591bc01a88fd3f27ed2bc3ae3c6b9f9
zeeshan-kodex <ahsansahi6@gmail.com>
2026-08-17 16:41:44 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Home from "./pages/Home";
 import Footer from "./components/layout/Footer/Footer";
+import Navbar from "./components/layout/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Home />
       <Footer />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/layout/Navbar/Navbar";
 import Hero from "../components/hero/Hero/Hero";
 import PopularDestinations from "../components/destinations/PopularDestinations/PopularDestinations";
 import TourActivities from "../components/activities/TourActivities/TourActivities";
@@ -9,7 +8,6 @@ import Newsletter from "../components/newsletter/Newsletter/Newsletter";
 const Home = () => {
   return (
     <>
-      <Navbar />
       <Hero />
       <PopularDestinations />
       <TourActivities />
